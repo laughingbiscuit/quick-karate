bf2efa7feef6b04b93b4b20623f2ead50ed0b394
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2023-01-06 14:47:11 +0000

    Update run.sh

--- helpers/run.sh
+++ helpers/run.sh
@@ -2,4 +2,4 @@
 set -e
  
 cp /tmp/FeaturesTest.java /karate/src/test/java/features/FeaturesTest.java
-DISPLAY=":1" mvn test -q
+DISPLAY=":1" mvn test -q -Dkarate.config.dir=/karate/src/test/java/features/
